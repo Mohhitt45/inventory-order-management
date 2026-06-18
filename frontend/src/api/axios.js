f773809454d44a8b0058f9dda2f4507bd58bcf65
Mohit Agrawal <agrawalmohit.ma@gmail.com>
2026-06-19 01:57:56 +0530

    update backend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "https://inventory-order-management-etq7.onrender.com/"
 });
 
 export default api;
