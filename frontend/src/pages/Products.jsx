70bba52ebdff1534cc66dc2532faf4bcd1ae9f2d
Mohit Agrawal <agrawalmohit.ma@gmail.com>
2026-06-19 01:19:30 +0530

    Initial working inventory project (FastAPI + React)

---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
+function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await api.get("/products/");
+        console.log("DATA:", res.data);
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+        setProducts([]);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h2>Products</h2>
+
+      <table className="table table-bordered mt-3">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>SKU</th>
+            <th>Price</th>
+            <th>Stock</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {products.length > 0 ? (
+            products.map((p) => (
+              <tr key={p.id}>
+                <td>{p.id}</td>
+                <td>{p.name}</td>
+                <td>{p.sku}</td>
+                <td>{p.price}</td>
+                <td>{p.stock_quantity}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5" className="text-center">
+                Loading / No Data
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Products;
