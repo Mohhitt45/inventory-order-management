9fbb8cb003f6c4921107f1a259a542f2ff3fdf08
Mohit Agrawal <agrawalmohit.ma@gmail.com>
2026-06-19 12:30:29 +0530

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine, Base
 from app import models
@@ -13,8 +14,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://inventory-order-management-lyart.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
